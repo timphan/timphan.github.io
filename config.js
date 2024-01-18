f6e9e38714cb7f585a7b9dcbc82ef5ef372d9257
Tim Phan <phan.timothyv@gmail.com>
2024-01-17 16:30:41 -0800

    boba so good
    yum

--- config.js
+++ config.js
@@ -9,10 +9,28 @@ var config = {
     byline: '',
     footer: 'Source: me',
     chapters: [
+               {
+            id: 'Summerfield',
+            alignment: 'center',
+            title: 'Summerfield Tea Bar (Garden Grove)',
+            image: 'https://s3-media0.fl.yelpcdn.com/bphoto/u38rV4PRzkGqkrRZWSkFeA/o.jpg',
+            description: 'The Strawberry Tornado one is my favorite',
+            location: {
+                center: [-117.94085, 33.78169],
+                zoom: 18.17,
+                pitch: 33.50,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
         {
             id: 'SunrightStudio',
-            alignment: 'full',
-            title: 'Sunright Studio',
+            alignment: 'left',
+            title: 'Sunright Studio (Garden Grove)',
             image: 'https://s3-media0.fl.yelpcdn.com/bphoto/4dvmES4Pb1oC-YpH_f4N5g/348s.jpg',
             description: 'White Peach Oolong Tea FTW',
             location: {
@@ -28,9 +46,15 @@ var config = {
             onChapterExit: []
         },
 
+
+
+
+
+
+
         {
             id: 'UrbanLocation',
-            alignment: 'full',
+            alignment: 'right',
             title: 'Urban Location',
             image: 'https://static.wixstatic.com/media/9d106b_f1e6e2ef5e42440eb2d1c7b7d60fa424~mv2_d_4686_3124_s_4_2.jpg/v1/fill/w_640,h_426,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/9d106b_f1e6e2ef5e42440eb2d1c7b7d60fa424~mv2_d_4686_3124_s_4_2.jpg',
             description: 'Good chicken wings and fries',
